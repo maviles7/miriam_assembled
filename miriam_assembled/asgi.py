1717015515ebf88dad7021ad8737fcb99b707d66
maviles <maviles221@gmail.com>
2024-10-15 16:43:49 -0700

    initial commit

---
+++ miriam_assembled/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for miriam_assembled project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miriam_assembled.settings')
+
+application = get_asgi_application()
