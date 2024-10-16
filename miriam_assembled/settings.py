c20559e7d92384ac6e42f94b4937c0065151d089
maviles <maviles221@gmail.com>
2024-10-15 17:39:08 -0700

    database connect (:

--- miriam_assembled/settings.py
+++ miriam_assembled/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+import environ
+env = environ.Env()
+env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,14 +76,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'miriam_assembled.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'miriamassembled',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
 
