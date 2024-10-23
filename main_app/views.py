b31a48d3692c9a5a21a5f463a8429d8631a8dfa7
maviles <maviles221@gmail.com>
2024-10-23 17:08:14 -0500

    home, about, & favicon

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello, Miriam!</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
