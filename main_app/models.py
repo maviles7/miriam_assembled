ac8e1bbc7480aebc3354f7124ffa317e9846f3b9
maviles <maviles221@gmail.com>
2024-10-24 13:38:32 -0500

    project model

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ class Projects(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     image = models.CharField(max_length=250)
-    deploy_link = models.CharField(max_length=500)
-    github_link = models.CharField(max_length=500)
+    deploy_link = models.URLField(max_length=500)
+    github_link = models.URLField(max_length=500)
 
     def __str__(self):
         return self.title
